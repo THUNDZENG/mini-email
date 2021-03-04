f1d34d842489f0b72c004ee99fd13e7ae2d1f6e8
ex-zenglei <zenglei@yodinfo.com>
2021-03-04 10:30:24 +0800

    feat：StringUtils中去除com.sun.istack.internal.Nullable注解；

--- src/main/java/io/github/thundzeng/miniemail/util/StringUtils.java
+++ src/main/java/io/github/thundzeng/miniemail/util/StringUtils.java
@@ -1,6 +1,5 @@
 package io.github.thundzeng.miniemail.util;
 
-import com.sun.istack.internal.Nullable;
 
 /**
  * 字符串工具类
@@ -8,7 +7,7 @@ import com.sun.istack.internal.Nullable;
  * @author thundzeng
  */
 public class StringUtils {
-    public static boolean isEmpty(@Nullable Object str) {
+    public static boolean isEmpty(Object str) {
         return (str == null || "".equals(str));
     }
 }
